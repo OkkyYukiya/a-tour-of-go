1b045e0918d0d7933b8d5cf3d16b4450d5079027
yukiya okita <mangaokky@icloud.com>
2025-10-12 23:10:33 +0900

    learn variables

---
+++ basics/var.go
@@ -0,0 +1,14 @@
+// package main
+
+// import "fmt"
+
+// // initializerを与えることが可能、その場合型宣言は省略可能
+// // var i, j = 1,2
+
+// // := 暗黙的な宣言はvar, funcなどが必要関数の外では利用不可能
+// func main () {
+// 	var i, j int = 1, 2
+// 	k := 3
+// 	c, python, java := true, false, "no!"
+// 	fmt.Println(i, j, k, c, python, java)	
+// }
