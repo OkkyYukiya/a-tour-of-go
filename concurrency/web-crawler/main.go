bdc616cee669330d11579029e6e65cc3e8e64fb1
yukiya okita <mangaokky@icloud.com>
2025-10-26 22:55:25 +0900

    update crawl

--- concurrency/web-crawler/main.go
+++ concurrency/web-crawler/main.go
@@ -2,37 +2,72 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
-
 type Fetcher interface {
 	// Fetch returns the body of URL and
 	// a slice of URLs found on that page.
 	Fetch(url string) (body string, urls []string, err error)
 }
 
+type SafeCache struct {
+	mu      sync.Mutex
+	visited map[string]bool
+}
+
+// Visit はURLを訪問済みとして記録、すでに訪問済みかどうか判定
+func (c *SafeCache) Visit(url string) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	
+	// すでに訪問済み
+	if c.visited[url] {
+		return false 
+	}
+	c.visited[url] = true
+	// 初めての訪問
+	return true 
+}
+
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
-func Crawl(url string, depth int, fetcher Fetcher) {
-	// TODO: Fetch URLs in parallel.
-	// TODO: Don't fetch the same URL twice.
-	// This implementation doesn't do either:
+func Crawl(url string, depth int, fetcher Fetcher, cache *SafeCache, wg *sync.WaitGroup) {
+	defer wg.Done()
+	
 	if depth <= 0 {
 		return
 	}
+	
+	// すでに訪問済みのURLはスキップ
+	if !cache.Visit(url) {
+		return
+	}
+	
 	body, urls, err := fetcher.Fetch(url)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	
 	fmt.Printf("found: %s %q\n", url, body)
+	
+	// 見つかったURLを並列でクロールする
 	for _, u := range urls {
-		Crawl(u, depth-1, fetcher)
+		wg.Add(1)
+		// goをつけてgoroutineに
+		go Crawl(u, depth-1, fetcher, cache, wg)
 	}
-	return
 }
 
 func main() {
-	Crawl("https://golang.org/", 4, fetcher)
+	cache := &SafeCache{visited: make(map[string]bool)}
+	var wg sync.WaitGroup
+	
+	wg.Add(1)
+	go Crawl("https://golang.org/", 4, fetcher, cache, &wg)
+	
+	wg.Wait()
+	fmt.Println("\n--- Crawling finished! ---")
 }
 
 // fakeFetcher is Fetcher that returns canned results.
